1c9b227a9ef70afd73e70ffdb3a088aad30deb36
Brandon Berney <brandonberney@protonmail.com>
2019-03-24 13:22:46 -0400

    I did it

--- array-matching/Lab7.Designer.cs
+++ array-matching/Lab7.Designer.cs
@@ -38,56 +38,63 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 22);
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(256, 25);
+            this.label1.Size = new System.Drawing.Size(128, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Words Per Minute (WPM)";
             // 
             // wpmInput
             // 
-            this.wpmInput.Location = new System.Drawing.Point(293, 19);
+            this.wpmInput.Location = new System.Drawing.Point(146, 10);
+            this.wpmInput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.wpmInput.Name = "wpmInput";
-            this.wpmInput.Size = new System.Drawing.Size(100, 31);
+            this.wpmInput.Size = new System.Drawing.Size(52, 20);
             this.wpmInput.TabIndex = 1;
             // 
             // gradeLabelStatic
             // 
             this.gradeLabelStatic.AutoSize = true;
-            this.gradeLabelStatic.Location = new System.Drawing.Point(120, 69);
+            this.gradeLabelStatic.Location = new System.Drawing.Point(60, 36);
+            this.gradeLabelStatic.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.gradeLabelStatic.Name = "gradeLabelStatic";
-            this.gradeLabelStatic.Size = new System.Drawing.Size(167, 25);
+            this.gradeLabelStatic.Size = new System.Drawing.Size(85, 13);
             this.gradeLabelStatic.TabIndex = 2;
             this.gradeLabelStatic.Text = "Grade Received";
             // 
             // calculateButton
             // 
-            this.calculateButton.Location = new System.Drawing.Point(80, 112);
+            this.calculateButton.Location = new System.Drawing.Point(40, 58);
+            this.calculateButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.calculateButton.Name = "calculateButton";
-            this.calculateButton.Size = new System.Drawing.Size(283, 50);
+            this.calculateButton.Size = new System.Drawing.Size(142, 26);
             this.calculateButton.TabIndex = 3;
             this.calculateButton.Text = "Calculate Student Grade";
             this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
             // 
             // gradeLetterLabel
             // 
             this.gradeLetterLabel.AutoSize = true;
-            this.gradeLetterLabel.Location = new System.Drawing.Point(293, 69);
+            this.gradeLetterLabel.Location = new System.Drawing.Point(146, 36);
+            this.gradeLetterLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.gradeLetterLabel.Name = "gradeLetterLabel";
-            this.gradeLetterLabel.Size = new System.Drawing.Size(26, 25);
+            this.gradeLetterLabel.Size = new System.Drawing.Size(33, 13);
             this.gradeLetterLabel.TabIndex = 4;
-            this.gradeLetterLabel.Text = "A";
+            this.gradeLetterLabel.Text = "None";
             // 
             // Lab7
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 174);
+            this.ClientSize = new System.Drawing.Size(212, 90);
             this.Controls.Add(this.gradeLetterLabel);
             this.Controls.Add(this.calculateButton);
             this.Controls.Add(this.gradeLabelStatic);
             this.Controls.Add(this.wpmInput);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Lab7";
             this.Text = "Lab 7";
             this.ResumeLayout(false);
--- array-matching/Lab7.cs
+++ array-matching/Lab7.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Grading ID: M5477
+ * Lab: 7
+ * Due Date: Mar 24 2019
+ * Course Section: 01
+ * Description: This program looks through arrays to determine a grade assignment
+ */
+ 
+using System;
 using System.Windows.Forms;
 
 namespace array_matching
@@ -15,6 +16,35 @@ namespace array_matching
         public Lab7()
         {
             InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            bool wpmTry; // A boolean for form verification
+            uint wpm; // The words per minute reported by the user
+
+            wpmTry = uint.TryParse(wpmInput.Text, out wpm);
+
+            if (wpmTry)
+            {
+                uint[] wpmMinThreshold = { 0, 15, 30, 50, 75 }; // Array of lowest wpm per bracket
+                string[] gradeReceived = { "F", "D", "C", "B", "A" }; // Array of related grades
+
+                for (int x = 0; x <= wpmMinThreshold.Length - 1; ++x)
+                {
+                    if (wpm > wpmMinThreshold[x])
+                    {
+                        gradeLetterLabel.Text = gradeReceived[x];
+                        x = wpmMinThreshold.Length; // Terminates loop early for efficiency
+                    } 
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please use a valid integer when stating the words per minute.", "Form Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
         }
     }
 }
